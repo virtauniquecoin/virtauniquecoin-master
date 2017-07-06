60673f95e01b01a674ad89fd7e8450664be47163
virtauniquecoin <support@virtauniquecoin.com>
2017-07-06 14:02:41 +0530

    change node

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-        {"195.181.247.173", "195.181.247.173"},
+        {"50.63.163.129", "50.63.163.129"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
